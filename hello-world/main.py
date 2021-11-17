1eaf40a3026c65d1a01b8655cf5f26959c7210b6
Nick <nick.gallois@hotmail.com>
2021-11-17 15:21:18 +0100

    ad folder

---
+++ hello-world/main.py
@@ -0,0 +1,19 @@
+# Do not modify these lines
+__winc_id__ = 'e75b6cd4a7404e3ca76c308566dafb5d'
+__human_name__ = 'hello-world'
+print ('qoute') 
+nick =2+9
+tymen = 'hallo tymen'
+print(nick)
+print(nick)
+print(nick + nick)
+print(5 * 5 + nick)
+print(tymen +str(nick))
+
+
+
+
+
+
+
+# Add your code after this line
